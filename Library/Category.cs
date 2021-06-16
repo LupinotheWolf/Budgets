1f0204e8a0d272a05e37d07c5399763165ccc1b3
LupinotheWolf <74966209+LupinotheWolf@users.noreply.github.com>
2021-06-16 12:01:09 +0100

    Init Comit

---
+++ Library/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum Category
+{
+    //Create category names here
+    Income,
+    bills,
+}
---
+++ Library/Transaction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Transaction
+{
+    public DateTime date;
+    public decimal amount;
+    public string title;
+
+}
